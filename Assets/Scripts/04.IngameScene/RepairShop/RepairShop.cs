c41054fb034cc2f0e30e4b033c8fbd71e1820333
phw0251 <phw0251@naver.com>
2025-05-01 13:20:29 +0900

    fix

--- Assets/Scripts/04.IngameScene/RepairShop/RepairShop.cs
+++ Assets/Scripts/04.IngameScene/RepairShop/RepairShop.cs
@@ -1,3 +1,4 @@
+//1
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
--- Assets/Scripts/04.IngameScene/RepairShop/RepairShopReceipt.cs
+++ Assets/Scripts/04.IngameScene/RepairShop/RepairShopReceipt.cs
@@ -1,3 +1,4 @@
+//1
 using System;
 using System.Collections;
 using System.Collections.Generic;
