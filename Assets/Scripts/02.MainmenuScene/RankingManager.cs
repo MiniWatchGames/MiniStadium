44086cafd67f24fd00ace1c065d70be4dd14e0cc
BORAMCHAM <boramcham.omo@gmail.com>
2025-05-08 12:53:34 +0900

    UI 에셋 적용

--- Assets/Scripts/02.MainmenuScene/RankingManager.cs
+++ Assets/Scripts/02.MainmenuScene/RankingManager.cs
@@ -6,18 +6,25 @@ using UnityEngine.UI;
 
 public class RankingManager : MonoBehaviour
 {
+    /*
     [Header("Ranking Scroll")]
     public GameObject rankingInfoPrefab;
     public Transform contentParent;
+    */
+
+    [Header("Button")] 
+    public Button closeButton;
 
     // 생성할 프리팹 개수
-    public int rankingCount = 100;
+    // public int rankingCount = 100;
 
     private void Start()
     {
-        CreateRankingInfo();
+        // CreateRankingInfo();
+        closeButton.onClick.AddListener(CloseRanking);
     }
 
+    /*
     private void CreateRankingInfo()
     {
         for (int i = 1; i <= rankingCount; i++)
@@ -28,5 +35,11 @@ public class RankingManager : MonoBehaviour
             // TextMeshProUGUI rankText = item.transform.Find("[Text] Rank").GetComponent<TextMeshProUGUI>();
             // rankText.text = i.ToString();
         }
+    }
+    */
+
+    private void CloseRanking()
+    {
+        gameObject.SetActive(false);
     }
 }
