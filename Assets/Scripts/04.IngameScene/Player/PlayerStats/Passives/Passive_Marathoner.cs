5a65a04c570f03b8c5670423d744cd23f954927f
phw0251 <phw0251@naver.com>
2025-05-10 22:57:34 +0900

    UI 최종 수정
    정비소, HUD 내 무기 마스킹 적용

--- Assets/Scripts/04.IngameScene/Player/PlayerStats/Passives/Passive_Marathoner.cs
+++ Assets/Scripts/04.IngameScene/Player/PlayerStats/Passives/Passive_Marathoner.cs
@@ -5,27 +5,8 @@ using UnityEngine;
 
 public class Passive_Marathoner : MonoBehaviour, IPassive
 {
-    Coroutine marathoner;
-
     public void ApplyPassive(PlayerController playerController)
     {
-        if (marathoner == null)
-        {
-            marathoner = StartCoroutine(Marathoner(playerController));
-        }
-    }
-
-    IEnumerator Marathoner(PlayerController playerController) {
-        while (true) {
-            if(playerController.ActionFsm.CurrentState == ActionState.Hit)
-                yield return new WaitForSeconds(3f);
-            if (playerController.CurrentHp.Value < playerController.BaseMaxHp.Value)
-                playerController.CurrentHp.Value += 5;
-            yield return new WaitForSeconds(1f);
-        }
-    }
-    private void OnDestroy()
-    {
-        StopAllCoroutines();
+        throw new NotImplementedException();
     }
 }
